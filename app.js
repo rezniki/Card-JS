4b367e1f9edc3c1ed053d565d16fb9bf82e8c31e
rezniki <igorreznik2001@gmail.com>
2023-08-04 23:34:01 +0300

    Created new object in file app.js

--- app.js
+++ app.js
@@ -29,38 +29,66 @@ let myCars = [
 
 let elemMyCars = document.querySelector('.my__cars');
 
-const funcMyCars = function(myCar) {
-    let elemData = document.createElement('div');
-    let elemImg = document.createElement('img');
-    let elemStamp = document.createElement('p');
-    let elemSpeed = document.createElement('p');
-    let elemRelease = document.createElement('p');
-    let elemCountry = document.createElement('p');
-    let elemWeight = document.createElement('p');
-
-    elemData.className = 'main__data';
-    elemImg.className = 'main__img'
-    elemStamp.className = 'main__stamp';
-    elemSpeed.className = 'main__speed';
-    elemRelease.className = 'main__release';
-    elemCountry.className = 'main__country';
-    elemWeight.className = 'main__weight';
-
-    elemImg.src = myCar.image;
-    elemStamp.textContent = myCar.stamp;
-    elemSpeed.textContent = `Speed: ${myCar.speed}`;
-    elemRelease.textContent = `Release: ${myCar.release}`;
-    elemCountry.textContent = `Country: ${myCar.country}`;
-    elemWeight.textContent = `Weight: ${myCar.weight}`;
-
-    elemData.append(elemImg);
-    elemData.append(elemStamp);
-    elemData.append(elemSpeed);
-    elemData.append(elemRelease);
-    elemData.append(elemCountry);
-    elemData.append(elemWeight);
-    elemMyCars.append(elemData);
+function renderCards() {
+    const funcMyCars = function(myCar) {
+        let elemData = document.createElement('div');
+        let elemImg = document.createElement('img');
+        let elemStamp = document.createElement('p');
+        let elemSpeed = document.createElement('p');
+        let elemRelease = document.createElement('p');
+        let elemCountry = document.createElement('p');
+        let elemWeight = document.createElement('p');
+    
+        elemData.className = 'main__data';
+        elemImg.className = 'main__img'
+        elemStamp.className = 'main__stamp';
+        elemSpeed.className = 'main__speed';
+        elemRelease.className = 'main__release';
+        elemCountry.className = 'main__country';
+        elemWeight.className = 'main__weight';
+    
+        elemImg.src = myCar.image;
+        elemStamp.textContent = myCar.stamp;
+        elemSpeed.textContent = `Speed: ${myCar.speed}`;
+        elemRelease.textContent = `Release: ${myCar.release}`;
+        elemCountry.textContent = `Country: ${myCar.country}`;
+        elemWeight.textContent = `Weight: ${myCar.weight}`;
+    
+        elemData.append(elemImg);
+        elemData.append(elemStamp);
+        elemData.append(elemSpeed);
+        elemData.append(elemRelease);
+        elemData.append(elemCountry);
+        elemData.append(elemWeight);
+        elemMyCars.append(elemData);
+    }
+    
+    myCars.forEach(funcMyCars);
 }
 
-myCars.forEach(funcMyCars);
+renderCards();
+
+let elemCharacter = document.querySelector('.main__character');
+let elemCharacterNumber = document.querySelector('.main__character__number');
+let elemButton = document.querySelector('.main__button');
+
+elemButton.onclick = function() {
+    let newElemCharacter = elemCharacter.value;
+    let newElemCharacterNumber = elemCharacterNumber.value; 
+    let newMyCar = 
+        {
+            stamp: newElemCharacter,
+            speed: newElemCharacterNumber,
+            release: newElemCharacterNumber,
+            country: newElemCharacter,
+            weight: newElemCharacterNumber,
+            image: newElemCharacter
+        };
+
+    myCars.push(newMyCar);
+    console.log(myCars);
+
+}
+
+
 
